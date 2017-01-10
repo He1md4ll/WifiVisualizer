af21142c6ca0820608783b7e910e488276e65d39
Martin Suckau <martin.suckau@web.de>
2017-01-10 19:03:21 +0100

    replaced delaunay triangulation with JTS implementation

--- app/src/main/java/edu/hsb/wifivisualizer/calculation/impl/IncrementalDelaunayService.java
+++ app/src/main/java/edu/hsb/wifivisualizer/calculation/impl/IncrementalDelaunayService.java
@@ -3,6 +3,11 @@ package edu.hsb.wifivisualizer.calculation.impl;
 import com.google.common.base.Function;
 import com.google.common.collect.Lists;
 import com.google.common.collect.Maps;
+import com.vividsolutions.jts.geom.Coordinate;
+import com.vividsolutions.jts.geom.Geometry;
+import com.vividsolutions.jts.geom.GeometryFactory;
+import com.vividsolutions.jts.geom.LinearRing;
+import com.vividsolutions.jts.triangulate.DelaunayTriangulationBuilder;
 
 import java.util.List;
 import java.util.Map;
@@ -10,39 +15,37 @@ import java.util.Map;
 import edu.hsb.wifivisualizer.calculation.IDelaunayService;
 import edu.hsb.wifivisualizer.model.Point;
 import edu.hsb.wifivisualizer.model.Triangle;
-import io.github.jdiemke.triangulation.DelaunayTriangulator;
-import io.github.jdiemke.triangulation.NotEnoughPointsException;
-import io.github.jdiemke.triangulation.Triangle2D;
-import io.github.jdiemke.triangulation.Vector2D;
 
 public class IncrementalDelaunayService implements IDelaunayService {
 
     @Override
     public List<Triangle> calculate(List<Point> triangleList) {
-        final Map<Vector2D, Point> vector2DPointMap = Maps.newLinkedHashMap();
+        final Map<Coordinate, Point> coordinatePointMap = Maps.newLinkedHashMap();
         final List<Triangle> result = Lists.newArrayList();
-        try {
-            final List<Vector2D> vector2DList = Lists.newArrayList(Lists.transform(triangleList, new Function<Point, Vector2D>() {
-                @Override
-                public Vector2D apply(Point input) {
-                    final Vector2D vector2D = new Vector2D(input.getPosition().latitude, input.getPosition().longitude);
-                    vector2DPointMap.put(vector2D, input);
-                    return vector2D;
-                }
-            }));
-            final DelaunayTriangulator triangulator = new DelaunayTriangulator(vector2DList);
-            triangulator.shuffle();
-            triangulator.triangulate();
-            result.addAll(Lists.transform(triangulator.getTriangles(), new Function<Triangle2D, Triangle>() {
-                @Override
-                public Triangle apply(Triangle2D input) {
-                    final Point a = vector2DPointMap.get(input.a);
-                    final Point b = vector2DPointMap.get(input.b);
-                    final Point c = vector2DPointMap.get(input.c);
-                    return new Triangle(Lists.newArrayList(a, b, c));
-                }
-            }));
-        } catch (NotEnoughPointsException ignored) {}
+
+        final List<Coordinate> coordinateList = Lists.transform(triangleList, new Function<Point, Coordinate>() {
+            @Override
+            public Coordinate apply(Point input) {
+                final Coordinate coordinate = new Coordinate(input.getPosition().latitude, input.getPosition().longitude);
+                coordinatePointMap.put(coordinate, input);
+                return coordinate;
+            }
+        });
+
+        final DelaunayTriangulationBuilder delaunayBuilder = new DelaunayTriangulationBuilder();
+        delaunayBuilder.setSites(coordinateList);
+        final Geometry triangles = delaunayBuilder.getTriangles(new GeometryFactory());
+
+        for (int i = 0; i < triangles.getNumGeometries(); i++) {
+            final List<Point> trianglePointList = Lists.newArrayList();
+            final Triangle triangle = new Triangle(trianglePointList);
+
+            LinearRing triangleRing = (LinearRing) triangles.getGeometryN(i).getBoundary();
+            for (int j = 0; j < 3; j++) {
+                trianglePointList.add(coordinatePointMap.get(triangleRing.getPointN(j).getCoordinate()));
+            }
+            result.add(triangle);
+        }
         return result;
     }
 }
